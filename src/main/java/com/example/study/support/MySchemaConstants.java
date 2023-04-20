33f0d46ae636a638ad63be703dc8e828921d1283
Gihun <563710@naver.com>
2023-04-20 19:59:04 +0900

    feat(projection): Chage Member -> User , SCHEMA(study->joara)

--- src/main/java/com/example/study/support/MySchemaConstants.java
+++ src/main/java/com/example/study/support/MySchemaConstants.java
@@ -1,8 +1,8 @@
 package com.example.study.support;
 
 public final class MySchemaConstants {
-	public static final String SCHEMA = "study_basic";
-	public static final String TB_MEMBER = "member";
+	public static final String SCHEMA = "joara_basic";
+	public static final String TB_USER = "user";
 	private MySchemaConstants() {
 	
 	}
