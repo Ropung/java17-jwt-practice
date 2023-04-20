501a6c2f4dd73ddea8fe6efa0d5bba54d244ad1c
Gihun <563710@naver.com>
2023-04-20 19:47:08 +0900

    fix(flyway): change schemas

--- src/main/resources/db/migration/V0.0.0__schema.sql
+++ src/main/resources/db/migration/V0.0.0__schema.sql
@@ -1,1 +1,15 @@
-CREATE SCHEMA IF NOT EXISTS study_basic;
+CREATE SCHEMA IF NOT EXISTS joara_basic;
+
+CREATE TABLE IF NOT EXISTS joara_basic.user (
+    id                  UUID,
+    email               VARCHAR(30)                         NOT NULL        UNIQUE,
+    password            VARCHAR(100)                        NOT NULL,
+    nickname            VARCHAR(30)                         NOT NULL        UNIQUE,
+    status              VARCHAR(30)                         NOT NULL,
+    created_at          DATE                                NOT NULL,
+    gender              CHAR(1)                             NOT NULL,
+
+    CONSTRAINT          pk_user_id                          PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX udx_user_email ON joara_basic.user(email);
