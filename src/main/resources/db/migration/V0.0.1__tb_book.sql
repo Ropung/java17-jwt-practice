b719e70e6a8ddcf8368a3868e8971b18d87004eb
Gihun <563710@naver.com>
2023-04-22 23:42:19 +0900

    feat(flyway): Write DDL

---
+++ src/main/resources/db/migration/V0.0.1__tb_book.sql
@@ -0,0 +1,41 @@
+
+
+--작품(BookGenre)장르 테이블
+CREATE TABLE IF NOT EXISTS joara_basic.book_genre (
+    id                  UUID                                PRIMARY KEY,
+    genre_name          VARCHAR(30)                         NOT NULL
+);
+
+--작품(Book)테이블,  SEQUENCE 추가
+CREATE TABLE IF NOT EXISTS joara_basic.book (
+    id                  UUID                                PRIMARY KEY,
+    member_id           UUID                                ,
+    genre_id            UUID                                NOT NULL,
+    isbn_cip            VARCHAR(30)
+    book_title          VARCHAR(30)                         NOT NULL,
+    book_description    TEXT                                NOT NULL,
+    book_cover          VARCHAR(255)                        ,
+    avg_score           INTEGER                             ,
+    create_at           DATE                                NOT NULL,
+    update_at           DATE                                ,
+    delete_at           DATE
+
+);
+
+--작품태그 목록(BookTag)테이블
+CREATE TABLE IF NOT EXISTS joara_basic.book_tag (
+    id                  UUID                                PRIMARY KEY,
+    book_id             UUID                                NOT NULL,
+    book_free_tag_id    UUID                                NOT NULL
+);
+--작품태그(BookFreeTag)테이블
+--CREATE TABLE IF NOT EXISTS joara_basic.book_tag (
+--    id                  UUID                                PRIMARY KEY,
+--    book_tag_name       VARCHAR(30)                         NOT NULL
+--);
+
+----작품(책) 디폴트 생성
+--INSERT INTO joara_basic.book(id,member_id, book_num,book_title, book_description)
+--VALUES ('ebce6e70-4768-48dc-9ee6-0867af44b2a1','62e9d74f-9524-4c14-92cb-0d2e47b86c01',nextval('seq_book'),'book_title1','book_description1');
+--INSERT INTO joara_basic.book(id,member_id, book_num,book_title, book_description)
+--VALUES ('ebce6e70-4768-48dc-9ee6-0867af44b2a2','62e9d74f-9524-4c14-92cb-0d2e47b86c02',nextval('seq_book'),'book_title1','book_description1');
---
+++ src/main/resources/db/migration/V0.0.2__tb_episode.sql
@@ -0,0 +1,46 @@
+
+--회차좋아요(EpiLove)
+CREATE TABLE IF NOT EXISTS joara_basic.epi_love (
+    id                  UUID                                PRIMARY KEY,
+    member_id           UUID                                ,
+    epi_id              UUID                                NOT NULL,
+    is_epi              BOOLEAN                             NOT NULL DEFAULT false
+);
+
+
+
+--회차(Epi)테이블,  SEQUENCE 추가
+CREATE TABLE IF NOT EXISTS joara_basic.epi (
+    id                  UUID                                PRIMARY KEY,
+    book_id             UUID                                NOT NULL,
+    epi_title           VARCHAR(30)                         NOT NULL,
+    epi_content         TEXT                                NOT NULL,
+    epi_review          VARCHAR(30)                         ,
+    epi_cover           VARCHAR(255)                        ,
+    epi_view_count      BIGINT                              DEFAULT 0,
+    create_at           DATE                                NOT NULL,
+    update_at           DATE                                ,
+    delete_at           DATE
+);
+
+
+--회차(EpiComment)댓글 테이블,  SEQUENCE 추가
+
+CREATE TABLE IF NOT EXISTS joara_basic.epi_reply (
+    id                  UUID                                PRIMARY KEY,
+    epi_id              UUID                                NOT NULL,
+    epi_reply_content   VARCHAR(255)                        NOT NULL,
+    create_at           DATE                                NOT NULL,
+    update_at           DATE                                ,
+    delete_at           DATE
+);
+
+--회차(EpiReply)댓글 테이블,  SEQUENCE 추가
+CREATE TABLE IF NOT EXISTS joara_basic.epi_sub_reply (
+    id                  UUID                                PRIMARY KEY,
+    epi_sub_reply_id    UUID                                NOT NULL,
+    epi_sub_reply_content   VARCHAR(255)                    NOT NULL,
+    create_at           DATE                                NOT NULL,
+    update_at           DATE                                ,
+    delete_at           DATE
+);
