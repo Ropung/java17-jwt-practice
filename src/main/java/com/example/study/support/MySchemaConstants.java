582a4a430546957177f83d85cbaded974054f306
Gihun <563710@naver.com>
2023-04-22 23:42:50 +0900

    feat(constants): Add TB name

--- src/main/java/com/example/study/support/MySchemaConstants.java
+++ src/main/java/com/example/study/support/MySchemaConstants.java
@@ -2,7 +2,17 @@ package com.example.study.support;
 
 public final class MySchemaConstants {
 	public static final String SCHEMA = "joara_basic";
-	public static final String TB_USER = "user";
+	public static final String TB_Refresh = "refresh_token";
+	public static final String TB_MEMBER = "member";
+	public static final String TB_FAVOR_GENRE = "member_favor_genre";
+	public static final String TB_FAVOR_BOOK = "member_favor_book";
+	public static final String TB_BOOK_GENRE = "book_genre";
+	public static final String TB_BOOK = "book";
+	public static final String TB_BOOK_TAG = "book_tag";
+	public static final String TB_EPI_LOVE = "epi_love";
+	public static final String TB_EPI = "epi";
+	public static final String TB_EPI_REPLY = "epi_reply";
+	public static final String TB_EPI_SUB_REPLY = "epi_sub_reply";
 	private MySchemaConstants() {
 	
 	}
